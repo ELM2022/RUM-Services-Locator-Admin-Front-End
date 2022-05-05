e4a06cfd4dd121348147798c7092ce39530c3520
Maria Vilanova <maria.vilanova@upr.edu>
2022-05-05 19:08:19 -0400

    Production setup (test 2)

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const publicPath = path.join(__dirname, 'public');
 
 app.use(cors());
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, 'build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+}
 
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-app.use(express.static(publicPath));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+// app.use(express.static(publicPath));
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(publicPath, 'index.html'));
+// });
 
 app.listen(port, () => console.log(`Server running on port ${port}.`));
